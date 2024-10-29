45e1b7fb9bfc4c2b2a5f0d1445152a7dbf4e1d53
Karthik <talupuri.saikarthik02@gmail.com>
2024-10-30 01:16:17 +0530

    Server CORS configuration update and vercel.json for vercel

--- server/Routes/routes.js
+++ server/Routes/routes.js
@@ -2,6 +2,9 @@ const express = require('express');
 const controller = require('../Controller/controller');
 const router = express.Router();
 
+router.get('/', (req, res) => {
+    res.status(200).json("Server running!");
+});
 router.get('/tickets/:id', controller.getTicketById);
 router.get('/tickets', controller.getTickets);
 router.put('/tickets/:id', controller.updateTicket);
